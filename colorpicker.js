7d35d33b9e4f218963605215907ba620d1311cb9
alishaan95 <81311759+alishaan95@users.noreply.github.com>
2021-03-24 20:43:20 +0530

    Add files via upload

---
+++ colorpicker.js
@@ -0,0 +1,52 @@
+document.getElementById('range-picker').addEventListener('click', function(e) {
+  var sizeList = document.getElementById('range-picker').children;
+  for (var i = 0; i <= sizeList.length - 1; i++) {
+    console.log(sizeList[i].classList);
+    if (sizeList[i].classList.contains('active')) {
+      sizeList[i].classList.remove('active');
+    }
+  }
+  e.target.classList.add('active');
+})
+
+
+document.getElementById('color-a').addEventListener('click', function () {
+    document.getElementById('color-overlay').style.transform = 'translateX(10px)';
+    document.getElementById('highlight-overlay').style.opacity = '1';
+    document.getElementById('highlight-overlay-mobile').style.opacity = '1';
+
+})
+document.getElementById('color-b').addEventListener('click', function () {
+    document.getElementById('color-overlay').style.transform = 'translateX(45px)';
+    document.getElementById('highlight-overlay').style.opacity = '0';
+    document.getElementById('highlight-overlay-mobile').style.opacity = '0';
+})
+
+
+//FOR SPEAKER
+
+document.getElementById('color-redspeak').addEventListener('click', function () {
+    document.getElementById('color-overlay-speaker').style.transform = 'translateX(10px)';
+    document.getElementById('highlight-overlayspeaker').style.opacity = '1';
+    document.getElementById('highlight-overlay-mobile').style.opacity = '1';
+
+})
+document.getElementById('color-bluespeak').addEventListener('click', function () {
+    document.getElementById('color-overlay-speaker').style.transform = 'translateX(45px)';
+    document.getElementById('highlight-overlayspeaker').style.opacity = '0';
+    document.getElementById('highlight-overlay-mobile').style.opacity = '0';
+})
+
+//FOR MUG
+
+document.getElementById('color-blackmug').addEventListener('click', function () {
+    document.getElementById('color-overlay-mug').style.transform = 'translateX(10px)';
+    document.getElementById('highlight-overlaymug').style.opacity = '1';
+    document.getElementById('highlight-overlay-mobile').style.opacity = '1';
+
+})
+document.getElementById('color-bluemug').addEventListener('click', function () {
+    document.getElementById('color-overlay-mug').style.transform = 'translateX(45px)';
+    document.getElementById('highlight-overlaymug').style.opacity = '0';
+    document.getElementById('highlight-overlay-mobile').style.opacity = '0';
+})
---
+++ stickyheader new header.js
@@ -0,0 +1,8 @@
+﻿$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 9) {
+        $(".newheader").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".newheader").removeClass("active");
+    }
+});
